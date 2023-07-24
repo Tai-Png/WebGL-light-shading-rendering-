c036ded6c9d80c78200aa583b019c296ac8f3787
tcp2 <tcp2@sfu.ca>
2023-07-24 14:17:32 -0700

    added in vColor so the color isn't permanently red. Added in pointLightProgram stuff. However, the cow is now not getting drawn and only the wireframe is getting drawn. Something to do with the buffer. Gotta fix that.

--- shadingCow.js
+++ shadingCow.js
@@ -2,20 +2,34 @@
 var canvas;
 var gl;
 var staticProgram;
+
+
+var iBuffer;
+var vBuffer;
+var vPosition;
+var vColor;
+var cowColor = vec4(1, 0.5, 0.5, 1);
 var cowVertices = get_vertices();
 let cowFaces = get_faces();
 
 cowFaces = flatten(cowFaces).map(function (element) {
     return element - 1;
 });
+console.log(cowFaces);
+var cowX = 0;
+var cowY = 0;
+var cowZ = 0;
+var initialCowX = 0;
+var initialCowY = 0;
+var initialCowZ = 0;
 
-var fudgeLocation
-var fudgeFactor
+var rotationX = 0;
+var rotationY = 0;
+var rotationZ = 0;
+var isRightMouseBtnPressed = false;
+var toggleRightMouseBtn = false;
 
 
-var iBuffer;
-var vBuffer;
-var vPosition;
 
 var MVP;
 var MVPlocation;
@@ -28,19 +42,23 @@ var projectionMatrix;
 var projectionMatrixLoc;
 var modelmatrix;
 
-var cowX = 0;
-var cowY = 0;
-var cowZ = 0;
-var initialCowX = 0;
-var initialCowY = 0;
-var initialCowZ = 0;
-var rotationX = 0;
-var rotationY = 0;
-var rotationZ = 0;
-var isRightMouseBtnPressed = false;
-var toggleRightMouseBtn = false;
 
 
+var pointLightProgram;
+var pointLightPos;
+var pointLightVBuffer;
+var pointLightIBuffer;
+var pointLightVPosition;
+var pointLightVertices = getPointLightVertices();
+var pointLightIndices = getPointLightIndices();
+console.log(pointLightIndices);
+var pointLightMVPlocation;
+var pointLightMVP = mat4(1, 0, 0, 0,
+                        0, 1, 0, 0,
+                        0, 0, 1, 0,
+                        0, 0, 0, 1);
+var pointLightVColor;
+var pointLightColor = vec4(0, 0, 0, 1);
 
 function resetCow() {
     cowX = initialCowX;
@@ -106,44 +124,61 @@ window.onload = function init() {
                 }
     });
 
-    staticProgram = initShaders( gl, "vertex-shader", "fragment-shader" ); 
     gl.enable(gl.DEPTH_TEST);
-    gl.useProgram( staticProgram );
 
+    // initShaders
+    staticProgram = initShaders( gl, "vertex-shader", "fragment-shader" );
+    pointLightProgram = initShaders( gl, "vertex-shader", "fragment-shader" );
+
+    // Static program buffer
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(cowVertices), gl.STATIC_DRAW); 
+    vPosition = gl.getAttribLocation( staticProgram, "vPosition" ); 
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 ); 
+    gl.enableVertexAttribArray( vPosition );
     iBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cowFaces), gl.STATIC_DRAW);
-
-    vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(cowVertices), gl.STATIC_DRAW); // This puts all the vertices into the buffer
-
-
-    vPosition = gl.getAttribLocation( staticProgram, "vPosition" ); 
-    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 ); // This means 3 things will be read in the buffer, namely (x, y, z)
-    gl.enableVertexAttribArray( vPosition );
-
     MVPlocation = gl.getUniformLocation(staticProgram, "MVP");
+    vColor = gl.getUniformLocation(staticProgram, "vColor");
 
-    initialCowX = cowX;
-    initialCowY = cowY;
-    initialCowZ = cowZ;
+    // pointLight program buffer
+    pointLightVBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pointLightVBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointLightVertices), gl.STATIC_DRAW);
+    pointLightVPosition = gl.getAttribLocation( pointLightProgram, "vPosition");
+    gl.vertexAttribPointer( pointLightVPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(pointLightVPosition);
+    pointLightIBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pointLightIBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(pointLightIndices), gl.STATIC_DRAW);
+    pointLightMVPlocation = gl.getUniformLocation(pointLightProgram, "MVP");
+    pointLightVColor = gl.getUniformLocation(pointLightProgram, "vColor");
 
 	render();
 }
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.clearColor( 0, 0, 0, 1 );
-    gl.useProgram(staticProgram);
+    gl.clearColor( 0, 0, 0, 0.5 );
+    
 
+    // staticProgram stuff
+    gl.useProgram(staticProgram);
+    gl.enableVertexAttribArray(vPosition);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.uniform4fv(vColor, cowColor);
+
+    // Cow math
     cam_pos = vec3(0, 0, 30);
     cow_pos = vec3(cowX, cowY, cowZ);
     cow_initial_pos = vec3(0, 0, 0)
-    
-    viewMatrix = lookAt(cam_pos, cow_initial_pos, vec3([0, 1, 0]));
-
     fov = 30;
+
+    viewMatrix = lookAt(cam_pos, cow_initial_pos, vec3([0, 1, 0]));
+    
     projectionMatrix = perspective(fov, canvas.width / canvas.height, 0.1, 100.0);
 
     modelmatrix = mat4(
@@ -158,7 +193,52 @@ function render() {
     MVP = mat4();
     MVP = mult(mult(projectionMatrix, viewMatrix), modelmatrix);
 
-    gl.uniformMatrix4fv(MVPlocation, false, flatten(MVP));
-
+    gl.uniformMatrix4fv(MVPlocation, false, flatten(MVP)); // send MVP to html
     gl.drawElements(gl.TRIANGLES, cowFaces.length, gl.UNSIGNED_SHORT, 0);
+    
+    // pointLightProgram stuff
+    gl.useProgram(pointLightProgram);
+    gl.enableVertexAttribArray(pointLightVPosition);
+    gl.bindBuffer(gl.ARRAY_BUFFER, pointLightVBuffer);
+    gl.vertexAttribPointer( pointLightVPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.uniform4fv(pointLightVColor, pointLightColor);
+
+    gl.uniformMatrix4fv(pointLightMVPlocation, false, flatten(pointLightMVP));
+
+    gl.drawElements(gl.LINES, pointLightVertices.length, gl.UNSIGNED_SHORT, 0);
+
+}
+
+function getPointLightVertices(){
+    let vertices = [
+        -1, -1, -1,
+         1, -1, -1,
+         1,  1, -1,
+        -1,  1, -1,
+        -1, -1,  1,
+         1, -1,  1,
+         1,  1,  1,
+        -1,  1,  1,
+    ];
+    
+    return vertices.map(e => {
+        return e/4
+    });
+}
+
+function getPointLightIndices(){
+    return [
+        0, 1,
+        1, 2,
+        2, 3,
+        3, 0,
+        4, 5,
+        5, 6,
+        6, 7,
+        7, 4,
+        0, 4,
+        1, 5,
+        2, 6,
+        3, 7,
+    ];
 }
